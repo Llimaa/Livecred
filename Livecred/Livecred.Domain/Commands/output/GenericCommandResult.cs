7239d9081088d52b55122beadef487ebd03db39e
limaa <limaa@MARCOS-PC>
2020-04-17 23:19:00 -0300

    Adicionando os comandos de entrada e saida

---
+++ Livecred/Livecred.Domain/Commands/output/GenericCommandResult.cs
@@ -0,0 +1,19 @@
+﻿using Livecred.Domain.Shared.Commands;
+namespace Livecred.Domain.Commands.output
+{
+    public class GenericCommandResult : ICommandOutput
+    {
+        public GenericCommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public GenericCommandResult() { }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
