ab676010625bcb98d0d04500569ee99af9e4dbe3
limaa <limaa@MARCOS-PC>
2020-04-17 22:19:26 -0300

    Adição dos objetos de valores Document, Name

---
+++ Livecred/Livecred.Domain/ValueObjects/Document.cs
@@ -0,0 +1,49 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Livecred.Domain.ValueObjects
+{
+    public class Document : Notifiable
+    {
+        public Document(string cpf = null)
+        {
+            CPF = cpf;
+            new Contract().
+                IsTrue(ValidarCPF(cpf), "CPF", "CPF Invalido");
+        }
+
+        public string CPF { get; set; }
+
+        private bool ValidarCPF(string cpf)
+        {
+            var multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            var tempCpf = cpf.Substring(0, 9);
+            var soma = 0;
+
+            for (var i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            var resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            var digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (var i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+    }
+}
---
+++ Livecred/Livecred.Domain/ValueObjects/Name.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace Livecred.Domain.ValueObjects
+{
+    public class Name : Notifiable
+    {
+        public Name(string firstName, string lastName)
+        {
+            new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O primeiro nome é obrigátorio")
+                .HasMinLen(LastName, 3, "FirstName", "O utimo nome é obrigátorio");
+
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
