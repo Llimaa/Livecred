124850de07d2e3bbcc427a60e7d6c8333dffde9a
limaa <limaa@MARCOS-PC>
2020-04-17 22:18:40 -0300

    Adição da entidadeBase

---
+++ Livecred/Livecred.Domain/Shared/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Livecred.Domain.Shared
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
