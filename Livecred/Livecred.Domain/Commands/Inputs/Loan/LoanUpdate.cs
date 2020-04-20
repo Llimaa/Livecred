e44dbe5b141e6173a0824ed4636e711790e13c0d
limaa <limaa@MARCOS-PC>
2020-04-20 15:24:29 -0300

    commando de entrada para Emprestimo

---
+++ Livecred/Livecred.Domain/Commands/Inputs/Loan/LoanUpdate.cs
@@ -0,0 +1,21 @@
+﻿using Flunt.Validations;
+using Livecred.Domain.Enuns;
+using Livecred.Domain.Shared.Commands;
+namespace Livecred.Domain.Commands.Inputs.Loan
+{
+    public class LoanUpdate : IcommandInput
+    {
+        public decimal Valor { get; set; }
+        public double Juro { get; set; }
+        public EStatusEmprestimo Status { get; set; }
+
+        public void Validate()
+        {
+            new Contract()
+               .Requires()
+               .HasMinLen(Valor.ToString(), 100, "Valor", "Valor minimo para realizar um emprestimo é R$:100")
+               .IsNotNullOrEmpty(Juro.ToString(), "Juro", "O campo juro não pode ficar em branco")
+               .IsNotNullOrEmpty(Status.ToString(), "Status", "O campo status não pode ficar em branco");
+        }
+    }
+}
