47f6378db1ecfd1a2c2969ac3939c3ea068d6344
limaa <limaa@MARCOS-PC>
2020-04-20 15:24:02 -0300

    comandos de entrada para Clientes

---
+++ Livecred/Livecred.Domain/Commands/Inputs/Client/ClientUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Validations;
+using Livecred.Domain.Shared.Commands;
+namespace Livecred.Domain.Commands.Inputs.Client
+{
+    public class ClientUpdate : IcommandInput
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NuberDocument { get; set; }
+        public string Telephone { get; set; }
+        public string Endereco { get; set; }
+
+        public void Validate()
+        {
+            new Contract()
+                .IsNotNullOrEmpty(FirstName, "Primeiro nome", "O campo do primeiro nome é obrigatório")
+                .IsNotNullOrEmpty(LastName, "Ultimo nome", "O campo do Ultimo nome é obrigatório")
+                .HasMinLen(NuberDocument, 11, "CPF", "Número de documento inválido")
+                .HasMaxLen(NuberDocument, 11, "CPF", "Número de documento inválido");
+        }
+    }
+}
