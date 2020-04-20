02f29584f55849809766830f2431bf751b70cea8
limaa <limaa@MARCOS-PC>
2020-04-20 18:59:34 -0300

    adição do comando para atualizar parcela

---
+++ Livecred/Livecred.Domain/Commands/Inputs/Parcela/ParcelaUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Livecred.Domain.Enuns;
+using Livecred.Domain.Shared.Commands;
+using System;
+namespace Livecred.Domain.Commands.Inputs.Parcela
+{
+    public class ParcelaUpdate : Notifiable, IcommandInput
+    {
+        public decimal Valor { get; set; }
+        public decimal ValorComJuro { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public double JuroAtraso { get; set; }
+        public EStatusParcela Status { get; set; }
+
+        public void Validate()
+        {
+            new Contract()
+                .IsNotNullOrEmpty(Valor.ToString(), "Valor", "O campo valor não pode esta em branco.")
+                .IsNotNullOrEmpty(DataVencimento.ToString(), "DataVencimento", "O campo Data de vencimento não pode esta em branco.")
+                .IsNotNullOrEmpty("EStatusParcela", "EStatusParcela", "O campo Status não pode esta em branco.");
+        }
+    }
+}
