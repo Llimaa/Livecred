ac2dea4c715e4c06e26a77eacf625c03e8b9b329
limaa <limaa@MARCOS-PC>
2020-04-21 16:06:17 -0300

    melhorias na entidade de emprestimo

--- Livecred/Livecred.Domain/Models/Loan.cs
+++ Livecred/Livecred.Domain/Models/Loan.cs
@@ -7,20 +7,22 @@ namespace Livecred.Domain.Models
 {
     public class Loan : EntityBase
     {
-        public Loan(decimal valor, double juro)
+        public Loan(decimal valor, double juro, Guid idClient)
         {
             Valor = valor;
             Juro = juro;
             Status = EStatusEmprestimo.Aberto;
-            CataCadastro = DateTime.Now;
+            DataCadastro = DateTime.Now;
+            IdClient = idClient;
         }
 
         public decimal Valor { get; private set; }
+        public Guid IdClient { get; private set; }
         public double Juro { get; private set; }
         public EStatusEmprestimo Status { get; private set; }
-        public DateTime CataCadastro { get; private set; }
+        public DateTime DataCadastro { get; private set; }
         public DateTime DataUpdate { get; private set; }
-        public IList<Parcela> Parcelas { get; set; }
+        public List<Parcela> Parcelas { get; set; }
 
         public void Update(decimal valor, double juro, EStatusEmprestimo status)
         {
@@ -33,6 +35,11 @@ namespace Livecred.Domain.Models
         public void AdicionarParcela(Parcela parcela)
         {
             this.Parcelas.Add(parcela);
+        }
+
+        public void AddRangeParcela(List<Parcela> parcelas)
+        {
+            this.Parcelas.AddRange(parcelas);
         }
 
         public void AtualizarParcela(Parcela parcela)
