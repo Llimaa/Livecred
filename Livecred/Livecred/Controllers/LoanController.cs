e8c87912c80309e68983ef5ef0cfd5daf1e3aeb6
limaa <limaa@MARCOS-PC>
2020-04-23 02:27:51 -0300

    adição do dontroller para os emprestimos

---
+++ Livecred/Livecred/Controllers/LoanController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livecred.Domain.Commands.Handlers;
+using Livecred.Domain.Commands.Inputs.Loan;
+using Livecred.Domain.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livecred.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly IRepositoryLoan _repositoryLoan;
+        private readonly LoanHandler _loanHandler;
+
+        private static Guid IdClient;
+
+        public LoanController(IRepositoryLoan repositoryLoan, LoanHandler loanHandler)
+        {
+            _repositoryLoan = repositoryLoan;
+            _loanHandler = loanHandler;
+        }
+
+        // GET: Loan
+        public async Task<ActionResult> Index(Guid IdCLient)
+        {
+            IdClient = IdCLient;
+            var loan = await _repositoryLoan.GetAllbyIdClient(IdCLient);
+            return View(loan);
+        }
+
+        // GET: Loan/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Loan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Loan/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(LoanInsert loanInsert)
+        {
+            try
+            {
+                loanInsert.Validate();
+                if (loanInsert.Valid)
+                {
+                    loanInsert.IdClient = IdClient;
+                    var res = await _loanHandler.Handler(loanInsert);
+                }
+
+                return RedirectToAction(nameof(Index), new { IdCLient = IdClient });
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
+        // GET: Loan/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Loan/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Loan/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Loan/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
