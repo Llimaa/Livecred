8cf6b1ace7be1b118578b5ff4c34fd5d818b44e7
limaa <limaa@MARCOS-PC>
2020-04-23 02:29:04 -0300

    adição controller para parcela

---
+++ Livecred/Livecred/Controllers/ParcelaController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livecred.Domain.Commands.Handlers;
+using Livecred.Domain.Commands.Inputs.Parcela;
+using Livecred.Domain.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livecred.Controllers
+{
+    public class ParcelaController : Controller
+    {
+
+        private readonly ParcelaHandler _parcelaHandler;
+        private readonly IRepositoryParcela _repositoryParcela;
+
+        private static Guid _idLoan;
+
+        public ParcelaController(ParcelaHandler parcelaHandler, IRepositoryParcela repositoryParcela)
+        {
+            _parcelaHandler = parcelaHandler;
+            _repositoryParcela = repositoryParcela;
+        }
+
+        // GET: Parcela
+        public async Task<ActionResult> Index(Guid IdLoan)
+        {
+            _idLoan = IdLoan;
+            var res = await _repositoryParcela.GetAllByIdLoan(IdLoan);
+            return View(res);
+        }
+
+        // GET: Parcela/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Parcela/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parcela/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ParcelaInput parcelaInput)
+        {
+            try
+            {
+                parcelaInput.IdLoan = _idLoan;
+                await _parcelaHandler.Handler(parcelaInput);
+                return RedirectToAction(nameof(Index), new { IdLoan = _idLoan });
+            }
+            catch(Exception e)
+            {
+                return View();
+            }
+        }
+
+        // GET: Parcela/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Parcela/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Parcela/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Parcela/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
