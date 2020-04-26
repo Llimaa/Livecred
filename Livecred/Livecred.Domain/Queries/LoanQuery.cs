98340c9d9a7dd30040b410605914f80df1122d26
limaa <limaa@MARCOS-PC>
2020-04-26 03:38:51 -0300

    adição libs ao projeto

---
+++ Livecred/Livecred.Domain/Queries/LoanQuery.cs
@@ -0,0 +1,19 @@
+﻿using Livecred.Domain.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Livecred.Domain.Queries
+{
+    public class LoanQuery
+    {
+        public Guid Id { get; set; }
+        public string NameClient { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Juro { get; set; }
+        public EStatusEmprestimo Status { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int TotalParcelas { get; set; }
+        public int TotalParcelasPagas { get; set; }
+    }
+}
---
+++ Livecred/Livecred/Configurations/HttpExtensionsValidate.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace Livecred.Configurations
+{
+    public static class HttpExtensionsValidate
+    {
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+                return false;
+            else if (request.Headers != null)
+                return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+            return false;
+        }
+    }
+}
