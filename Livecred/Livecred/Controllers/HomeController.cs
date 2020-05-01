15f14fe71b374991e70a74c2e1122af618ac1a00
limaa <limaa@MARCOS-PC>
2020-04-30 22:50:59 -0300

    removendo codigo sujo no no controller/visualização home

--- Livecred/Livecred/Controllers/HomeController.cs
+++ Livecred/Livecred/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Livecred.Models;
 using Livecred.Domain.Repositories;
 using Livecred.Domain.Enuns;
 using System.Globalization;
@@ -30,7 +25,6 @@ namespace Livecred.Controllers
 
             var loanQuery = await _repositoryLoan.GetLoanQuery(EStatusEmprestimo.Aberto);
             ViewBag.TotalemprestimosAbertos = await _repositoryLoan.GetTotalOrderByStatus(EStatusEmprestimo.Aberto);
-            // ViewBag.emprestimos = await _repositoryLoan.GetAll();
             ViewBag.ValorRecebido = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", await _repositoryParcela.GetTotalRecebido(EStatusParcela.Paga));
             ViewBag.valorTotalDeEmprestimos = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", await _repositoryLoan.GetMoneyTotal());
             ViewBag.emprestimosDoUtimoMes = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", await _repositoryLoan.GetTotalByThirtyToday(DateTime.Now.AddMonths(-1), DateTime.Now));
@@ -42,17 +36,6 @@ namespace Livecred.Controllers
                 return PartialView("Index", loanQuery);
 
             return View(loanQuery);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
